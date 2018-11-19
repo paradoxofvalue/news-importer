a8ca89a10faac860514671e7be05b4591f315d88
Vadym Lavorchuk <lavorchukvadim@gmail.com>
2018-11-20 00:22:06 +0200

    add sites configuration, add ability to save post as a draft

--- news-importer.php
+++ news-importer.php
@@ -27,7 +27,7 @@ add_action('admin_menu', 'news_importer_menu');
 function news_importer_menu() {
 
 	//create new top-level menu
-	add_menu_page('News Importer Settings', 'NI Settings', 'administrator', __FILE__, 'ni_settings_page',plugins_url('/images/icon.png', __FILE__));
+	add_menu_page('News Importer Settings', 'NI Settings', 'administrator', __FILE__, 'ni_settings_page',plugins_url('/ni.svg', __FILE__));
 
 	//call register settings function
 	add_action( 'admin_init', 'register_mysettings' );
@@ -37,52 +37,126 @@ function news_importer_menu() {
 function register_mysettings() {
 	//register our settings
 	register_setting( 'ni-settings-group', 'ni-status' );
-	register_setting( 'ni-settings-group', 'ni-site-1' );
-	register_setting( 'ni-settings-group', 'ni-site-2' );
-	register_setting( 'ni-settings-group', 'ni-site-3' );
-	register_setting( 'ni-settings-group', 'ni-site-4' );
-	register_setting( 'ni-settings-group', 'ni-site-5' );
+
+	register_setting( 'ni-settings-group', 'ni-site-url-1' );
+	register_setting( 'ni-settings-group', 'ni-site-title-1' );
+	register_setting( 'ni-settings-group', 'ni-site-pagination-1' );
+	register_setting( 'ni-settings-group', 'ni-site-news-title-1' );
+	register_setting( 'ni-settings-group', 'ni-site-news-text-1' );
+	register_setting( 'ni-settings-group', 'ni-site-news-img-1' );
+
+	register_setting( 'ni-settings-group', 'ni-site-url-2' );
+	register_setting( 'ni-settings-group', 'ni-site-title-2' );
+	register_setting( 'ni-settings-group', 'ni-site-pagination-2' );
+	register_setting( 'ni-settings-group', 'ni-site-news-title-2' );
+	register_setting( 'ni-settings-group', 'ni-site-news-text-2' );
+	register_setting( 'ni-settings-group', 'ni-site-news-img-2' );
+
+	register_setting( 'ni-settings-group', 'ni-site-url-3' );
+	register_setting( 'ni-settings-group', 'ni-site-title-3' );
+	register_setting( 'ni-settings-group', 'ni-site-pagination-3' );
+	register_setting( 'ni-settings-group', 'ni-site-news-title-3' );
+	register_setting( 'ni-settings-group', 'ni-site-news-text-3' );
+	register_setting( 'ni-settings-group', 'ni-site-news-img-3' );
+
+	register_setting( 'ni-settings-group', 'ni-site-url-4' );
+	register_setting( 'ni-settings-group', 'ni-site-title-4' );
+	register_setting( 'ni-settings-group', 'ni-site-pagination-4' );
+	register_setting( 'ni-settings-group', 'ni-site-news-title-4' );
+	register_setting( 'ni-settings-group', 'ni-site-news-text-4' );
+	register_setting( 'ni-settings-group', 'ni-site-news-img-4' );
+
 }
 
 function ni_settings_page() {
 ?>
 <div class="wrap">
-<h2>News Importer Settings</h2>
+  <h2>News Importer Settings</h2>
+  <form method="post" action="options.php">
+      <span>Статус: <?= get_option('ni-status');?></span>
+      <?php settings_fields( 'ni-settings-group' ); ?>
+      <table class="form-table">
+         <thead>
+            <th>Website</th>
+            <th>Link tag to news</th>
+            <th>Tag to pagination</th>
+            <th>News title tag</th>
+            <th>News content tag</th>
+         </thead>
+         <tr>
+            <td>
+               <input type="text" name="ni-site-url-1" value="<?php echo get_option('ni-site-url-1'); ?>" />
+            </td>
+            <td>
+              <input type="text" name="ni-site-title-1" value="<?php echo get_option('ni-site-title-1'); ?>" />
+            </td>
+            <td>
+              <input type="text" name="ni-site-pagination-1" value="<?php echo get_option('ni-site-pagination-1'); ?>" />
+            </td>
+            <td>
+              <input type="text" name="ni-site-news-title-1" value="<?php echo get_option('ni-site-news-title-1'); ?>" />
+            </td>
+            <td>
+              <input type="text" name="ni-site-news-text-1" value="<?php echo get_option('ni-site-news-text-1'); ?>" />
+            </td>
+         </tr>
+         <tr >
+            <td>
+              <input type="text" name="ni-site-url-2" value="<?php echo get_option('ni-site-url-2'); ?>" />
+            </td>
+            <td>
+              <input type="text" name="ni-site-title-2" value="<?php echo get_option('ni-site-title-2'); ?>" />
+            </td>
+            <td>
+              <input type="text" name="ni-site-pagination-2" value="<?php echo get_option('ni-site-pagination-2'); ?>" />
+            </td>
+            <td>
+              <input type="text" name="ni-site-news-title-2" value="<?php echo get_option('ni-site-news-title-2'); ?>" />
+            </td>
+            <td>
+              <input type="text" name="ni-site-news-text-2" value="<?php echo get_option('ni-site-news-text-2'); ?>" />
+            </td>
+         </tr>
+         <tr >
+            <td>
+              <input type="text" name="ni-site-url-3" value="<?php echo get_option('ni-site-url-3'); ?>" />
+            </td>
+            <td>
+              <input type="text" name="ni-site-title-3" value="<?php echo get_option('ni-site-title-3'); ?>" />
+            </td>
+            <td>
+              <input type="text" name="ni-site-pagination-3" value="<?php echo get_option('ni-site-pagination-3'); ?>" />
+            </td>
+            <td>
+              <input type="text" name="ni-site-news-title-3" value="<?php echo get_option('ni-site-news-title-3'); ?>" />
+            </td>
+            <td>
+              <input type="text" name="ni-site-news-text-3" value="<?php echo get_option('ni-site-news-text-3'); ?>" />
+            </td>
+         </tr>
+         <tr >
+            <td>
+              <input type="text" name="ni-site-url-4" value="<?php echo get_option('ni-site-url-4'); ?>" />
+            </td>
+            <td>
+              <input type="text" name="ni-site-title-4" value="<?php echo get_option('ni-site-title-4'); ?>" />
+            </td>
+            <td>
+              <input type="text" name="ni-site-pagination-4" value="<?php echo get_option('ni-site-pagination-4'); ?>" />
+            </td>
+            <td>
+              <input type="text" name="ni-site-news-title-4" value="<?php echo get_option('ni-site-news-title-4'); ?>" />
+            </td>
+            <td>
+              <input type="text" name="ni-site-news-text-4" value="<?php echo get_option('ni-site-news-text-4'); ?>" />
+            </td>
+         </tr>
+      </table>
 
-<form method="post" action="options.php">
-    <span>Статус: <?= get_option('ni-status');?></span>
-    <?php settings_fields( 'ni-settings-group' ); ?>
-    <table class="form-table">
-        <tr valign="top">
-        <th scope="row">website #1</th>
-        <td><input type="text" name="ni-site-1" value="<?php echo get_option('ni-site-1'); ?>" /></td>
-        </tr>
-         
-        <tr valign="top">
-        <th scope="row">website #2</th>
-        <td><input type="text" name="ni-site-2" value="<?php echo get_option('ni-site-2'); ?>" /></td>
-        </tr>
-        
-        <tr valign="top">
-        <th scope="row">website #3</th>
-        <td><input type="text" name="ni-site-3" value="<?php echo get_option('ni-site-3'); ?>" /></td>
-        </tr>
-
-        <tr valign="top">
-        <th scope="row">website #4</th>
-        <td><input type="text" name="ni-site-4" value="<?php echo get_option('ni-site-4'); ?>" /></td>
-        </tr>
-        <tr valign="top">
-        <th scope="row">website #5</th>
-        <td><input type="text" name="ni-site-5" value="<?php echo get_option('ni-site-5'); ?>" /></td>
-        </tr>
-    </table>
-    
-    <p class="submit">
-    <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
-    </p>
-
-</form>
+      <p class="submit">
+        <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+      </p>
+  </form>
 </div>
 <?php } ?>
 
--- scrapper.js
+++ scrapper.js
@@ -4,153 +4,178 @@ const needle = require('needle');
 const cheerio = require('cheerio');
 
 const sequelize = new Sequelize('wp', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql',
+  host: 'localhost',
+  dialect: 'mysql',
 });
 
 sequelize
     .query(
-        `SELECT option_value FROM wp_options WHERE option_name LIKE "ni-site-%"`,
+        `SELECT option_name, option_value FROM wp_options WHERE option_name LIKE "ni-site-%"`,
     )
     .then(sites => {
-        // new Parser(sites[0].map(site => {
-        //     return site.option_value;
-        // }));
-        new Parser([sites[0][0].option_value]);
+      let websitesConfigurations = [
+        {
+          websiteUrl: '',
+          linksSelector: '',
+          paginationLinksSelector: '',
+          titleSelector: '',
+          contentSelector: '',
+        }
+      ];
+      console.info('Data fetched from WP!');
+      new Parser(sites[0]);
     });
 
 class Parser {
-    constructor(websitesList) {
-        this.websitesList = websitesList;
-        this.paginates = tress(this.performPaginates.bind(this));
-        this.news = tress(this.performNews);
+  constructor(websitesList) {
+    console.info('Parser constructor!');
+    this.websitesList = this.performDataFromWPDB(websitesList);
+    this.paginates = tress(this.performPaginates.bind(this));
+    this.news = tress(this.performNews.bind(this));
+    console.info('Websites List:', this.websitesList);
+    this.currentWebsite = {};
 
-        this.news.drain = this.finished('news');
+    // this.websitesList.forEach(websiteConfiguration => {
+      let websiteConfiguration = this.websitesList[1];
+      this.currentWebsite = websiteConfiguration;
+      console.info('Website taken:', this.currentWebsite);
+      if (websiteConfiguration && websiteConfiguration.websiteUrl) {
+        needle('get', websiteConfiguration.websiteUrl)
+            .then((res) => {
+              let $ = cheerio.load(res.body);
 
-        this.websitesList.forEach(websiteUrl => {
-            if (websiteUrl) {
-                needle('get', websiteUrl)
-                    .then((res) => {
-                        let $ = cheerio.load(res.body);
-                        /**
-                         * получаем новости
-                         */
-                        if ($('h3 a').length > 5) {
-                            $('h3 a').each((i, elem) => {
-                                this.news.push($(elem).attr('href'));
-                            })
-                        }
-                        if ($('h2 a').length > 5) {
-                            $('h2 a').each((i, elem) => {
-                                this.news.push($(elem).attr('href'));
-                            })
-                        }
-                        if ($('h1 a').length > 5) {
-                            $('h1 a').each((i, elem) => {
-                                this.news.push($(elem).attr('href'));
-                            })
-                        }
+              if ($(`${this.currentWebsite.linksSelector}`).length > 5) {
+                $(`${this.currentWebsite.linksSelector}`).each((i, elem) => {
+                  this.news.push($(elem).attr('href'));
+                });
+              }
 
-                        /**
-                         * получаем пагинацию
-                         */
-                        if ($(`[class*="pag"] a`).length > 5 && $(`[class*="pag"] a`).length < 20) {
-                            $(`[class*="pag"] a`).each((i, elem) => {
-                                this.paginates.push($(elem).attr('href'));
-                            })
-                        }
-                    })
-                    .catch((err) => {
-                        debugger;
-                    })
-            }
+              if (this.currentWebsite.paginationLinksSelector) {
+                if ($(`${this.currentWebsite.paginationLinksSelector}`).length > 5 &&
+                    $(`${this.currentWebsite.paginationLinksSelector}`).length < 50) {
+                  $(`${this.currentWebsite.paginationLinksSelector}`).each((i, elem) => {
+                    this.paginates.push($(elem).attr('href'));
+                  })
+                }
+              }
+
+            })
+            .catch((err) => {
+              debugger;
+            })
+      }
+    // });
+
+    // this.news.drain = this.finished('news');
+  }
+
+  performDataFromWPDB(array) {
+    let tempArray = [];
+    array.forEach((item, key) => {
+      let index = item.option_name.substr(item.option_name.length - 1, item.option_name.length);
+      if (item.option_name.length && !tempArray[index]) {
+        tempArray[index] = {};
+      }
+      if (item.option_name.includes('ni-site-url')) {
+        tempArray[index]['websiteUrl'] = item.option_value;
+      }
+      if (item.option_name.includes('ni-site-title')) {
+        tempArray[index]['linksSelector'] = item.option_value;
+      }
+      if (item.option_name.includes('ni-site-pagination')) {
+        tempArray[index]['paginationLinksSelector'] = item.option_value;
+      }
+      if (item.option_name.includes('ni-site-news-title')) {
+        tempArray[index]['titleSelector'] = item.option_value;
+      }
+      if (item.option_name.includes('ni-site-news-text')) {
+        tempArray[index]['contentSelector'] = item.option_value;
+      }
+    });
+    return tempArray;
+  }
+
+  performPaginates(url, callback) {
+    needle('get', url)
+        .then((res) => {
+          let $ = cheerio.load(res.body);
+          if ($(`${this.currentWebsite.linksSelector}`).length > 5) {
+            $(`${this.currentWebsite.linksSelector}`).each((i, elem) => {
+              this.news.push($(elem).attr('href'));
+            });
+          }
+          callback();
+        })
+        .catch((err) => {
+          debugger;
+        })
+  }
+
+  performNews(newsUrl, callback) {
+    needle('get', newsUrl)
+        .then((res) => {
+          let $ = cheerio.load(res.body),
+              title = '',
+              text = '',
+              images = [],
+              iframes = [];
+
+          if ($(`${this.currentWebsite.titleSelector}`).length) {
+            title = $(`${this.currentWebsite.titleSelector}`)[0];
+            title = $(title).text();
+          }
+          if ($(`${this.currentWebsite.contentSelector}`).length) {
+            text = $(`${this.currentWebsite.contentSelector}`).text();
+          }
+
+          if ($(`${this.currentWebsite.contentSelector} img`).length) {
+            $(`${this.currentWebsite.contentSelector} img`).each((i, elem) => {
+              images.push({
+                src: $(elem).attr('src'),
+                alt: $(elem).attr('alt')
+              })
+            })
+          }
+
+          if ($(`${this.currentWebsite.contentSelector} iframe`).length) {
+            $(`${this.currentWebsite.contentSelector} iframe`).each((i, elem) => {
+              let src = $(elem).attr('src'),
+                  type = '';
+              if ($(`${this.currentWebsite.contentSelector} iframe`).attr('src').match(/^(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/)) {
+                type = 'youtube';
+              }
+              iframes.push({
+                src: src,
+                type: type,
+              })
+            })
+          }
+          console.log(title, text, images);
+          this.saveToDraft({title, text, images, iframes}, callback);
 
         })
-    }
+        .catch((err) => {
+          debugger;
+        })
 
-    performPaginates(url, callback) {
-        let self = this;
-        needle('get', url)
-            .then((res) => {
-                let $ = cheerio.load(res.body);
-                /**
-                 * получаем новости n2
-                 */
-                if ($('h3 a').length > 5) {
-                    $('h3 a').each((i, elem) => {
-                        self.news.push($(elem).attr('href'));
-                    })
-                }
-                if ($('h2 a').length > 5) {
-                    $('h2 a').each((i, elem) => {
-                        self.news.push($(elem).attr('href'));
-                    })
-                }
-                if ($('h1 a').length > 5) {
-                    $('h1 a').each((i, elem) => {
-                        self.news.push($(elem).attr('href'));
-                    })
-                }
-              callback();
+  }
 
-            })
-            .catch((err) => {
-                debugger;
-            })
-    }
+  saveToDraft(data, callback) {
+    let date = new Date(),
+        dateString = `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    sequelize
+        .query(
+            `INSERT INTO wp_posts (ID, post_author, post_date, post_date_gmt, post_content, post_title, post_excerpt, post_status, comment_status, ping_status, post_password, post_name, to_ping, pinged, post_modified, post_modified_gmt, post_content_filtered, post_parent, guid, menu_order, post_type, post_mime_type, comment_count) VALUES ('0', '1', '${dateString}', '${dateString}', '${data.text}', '${data.title}', '', 'draft', 'open', 'open', '', '', '', '', '${dateString}', '${dateString}', '', '0', '', '0', 'post', '', '0')`,
+        )
+        .then(res => {
+          debugger;
+          callback();
+        });
 
-    performNews(newsUrl, callback) {
-        needle('get', newsUrl)
-            .then((res) => {
-                let $ = cheerio.load(res.body),
-                    title = '',
-                    text = '',
-                    images = [],
-                    iframes = [];
-                if ($('h1').length) {
-                    title = $('h1')[0];
-                    title = $(title).text();
-                }
-                /**
-                 * для dailytechinfo - просто шикарное форматирование...
-                 */
-                if ($('.content [id*="news"]').length) {
-                    text = $('.content [id*="news"]').text();
-                }
-                if ($('.content [id*="news"] img').length) {
-                    $('.content [id*="news"] img').each((i, elem) => {
-                        images.push({
-                            src: $(elem).attr('src'),
-                            alt: $(elem).attr('alt')
-                        })
-                    })
-                }
-                if ($('.content [id*="news"] iframe').length) {
-                    $('.content [id*="news"] iframe').each((i, elem) => {
-                        let src = $(elem).attr('src'),
-                            type = '';
-                        if ($('.content [id*="news"] iframe').attr('src').match(/^(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/)) {
-                            type = 'youtube';
-                        }
-                        iframes.push({
-                            src: src,
-                            type: type,
-                        })
-                    })
-                }
+  }
 
-                console.log(title, text, images);
-
-                callback();
-
-            })
-            .catch((err) => {
-                debugger;
-            })
-
-    }
-    finished(type) {
-      debugger
-      console.log(`${type} Done!!!`);
-    }
+  finished(type) {
+    debugger;
+    console.log(`${type} Done!!!`);
+  }
 }
